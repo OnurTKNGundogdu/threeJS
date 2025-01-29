84d0ad6c9a389a893ffece1dc317a76082210def
Onur Tekin Gundogdu <onurgundogdu@btcsoftware.co.uk>
2025-01-29 00:04:47 +0000

    google/model viewer added

--- src/component/Content.jsx
+++ src/component/Content.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Card, Row, Col } from 'antd';
 import { foodsAndDescs } from '../constants/foodsAndDesc';
-import ThreeDScene from '../component/ThreeDScene';
-import * as THREE from "three";
+import '@google/model-viewer';
 
 const renderDetail = (item, i) => {
-    const renderer = new THREE.WebGLRenderer({ alpha: true });
   return (
     <Card key={i} id={item.label && `#${item.label}`} className='container-card' title={item.title}>
       <Row style={{ flexDirection: item.isReverse && 'row-reverse' }} gutter={[24, 24]}>
         <Col span={14}>{item.desc}</Col>
         <Col span={9} style={{ background: '#9ad284' }}>
-          <ThreeDScene modelName={item.modelName} key2={i} renderer={renderer} />
+          <model-viewer
+            src={`./models/${item.modelName}/scene.gltf`}
+            alt='A 3D model'
+            auto-rotate
+            key2={i}
+            camera-controls
+            style={{ width: '100%', height: '500px' }}
+          ></model-viewer>
         </Col>
       </Row>
     </Card>
