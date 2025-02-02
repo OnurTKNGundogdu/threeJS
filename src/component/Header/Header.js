b120bd6d9b6a3d1cad597a2620e32a82c2ff5532
Onur Tekin Gundogdu <onurgundogdu@btcsoftware.co.uk>
2025-02-02 00:51:18 +0000

    new updated

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,39 +1,38 @@
 import React from 'react';
 import './Header.css';
 import { WhatsAppOutlined } from '@ant-design/icons';
-import { Col, Row, Typography } from 'antd';
+import { Typography } from 'antd';
 
-const Header = () => {
+export const Header = () => {
   return (
     <header className='header'>
-      <Row style={{ width: '100%' }}>
-        <Col span={14} style={{ margin: '1%', textAlign: 'end', alignContent: 'end' }}>
-          <Typography className='header-title'>Restaurant Name</Typography>
-        </Col>
-
-        <Col span={9} style={{ margin: '1%' }}>
-          <Row align={'bottom'} justify={'end'} style={{ width: '100%' }}>
-            <Col>
-              <Typography className='footer-text' id='footerText'>
-                Contact Us: 0776-025-9130
-              </Typography>
-            </Col>
-            <Col>
-              <a
-                href='https://wa.me/07760259130'
-                className='wp-icon'
-                id='wpIcon'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <WhatsAppOutlined style={{ fontSize: '40px', color: '#9A3D3D' }} />
-              </a>
-            </Col>
-          </Row>
-        </Col>
-      </Row>
+      <div
+        style={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          padding: '10px 20px',
+        }}
+      >
+        <Typography style={{ flex: 1, left: '50%', textAlign: 'center' }} className='header-title'>
+          Restaurant Name
+          <div style={{ display: 'inline-flex' }}>
+            <Typography className='footer-text' id='footerText'>
+              Contact Us: 0776-025-9130
+            </Typography>
+            <a
+              href='https://wa.me/07760259130'
+              className='wp-icon'
+              id='wpIcon'
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <WhatsAppOutlined style={{ fontSize: '40px', color: '#9A3D3D' }} />
+            </a>
+          </div>
+        </Typography>
+      </div>
     </header>
   );
 };
-
-export default Header;
