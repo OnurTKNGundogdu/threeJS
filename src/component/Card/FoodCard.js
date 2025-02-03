f122ebcc77d8e4fc831e06658766d7e4cf77127c
Onur Tekin Gundogdu <onurgundogdu@btcsoftware.co.uk>
2025-02-03 01:41:56 +0000

    test

--- src/component/Card/FoodCard.js
+++ src/component/Card/FoodCard.js
@@ -11,7 +11,7 @@ const FoodCard = ({ item, on3DClick }) => {
       className='food-card'
       cover={<img alt={item.name} src={item.image} onClick={on3DClick} className='food-card-image' />}
     >
-      <p className='food-card-description'>{item.description}</p>
+      <p className='food-card-description'>{item.description ?? ` `}</p>
       <div className='food-card-footer'>
         <Typography className='food-card-price'>{item.price}</Typography>
         <EyeOutlined className='food-card-icon' onClick={on3DClick} />
