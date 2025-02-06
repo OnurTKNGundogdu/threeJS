2e3524e0a84c65bfe5cca80f6d24b1173f37acf6
Sezer KESTI <sezerkesti@Sezers-Mac-mini.local>
2025-02-06 23:49:44 +0000

    phone icon

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import './Header.css';
-import { WhatsAppOutlined } from '@ant-design/icons';
+import { WhatsAppOutlined, PhoneOutlined } from '@ant-design/icons';
 import { Typography } from 'antd';
 
 export const Header = () => {
   return (
     <header className='header'>
       <div className='header-content'>
-        <div className='header-title'>The Gyros & Kebabs Bar</div>
+        <div className='header-title'>The Gyros & Kebabs</div>
         <div className='contact-info'>
           <Typography className='footer-text' id='footerText'>
             Contact Us: 0776-025-9130
           </Typography>
           <a
-            href='https://wa.me/+447760259130'
-            className='wp-icon'
-            id='wpIcon'
-            target='_blank'
-            rel='noopener noreferrer'
+              href='https://wa.me/+447909225031'
+              className='wp-icon'
+              id='wpIcon'
+              target='_blank'
+              rel='noopener noreferrer'
           >
-            <WhatsAppOutlined style={{ fontSize: '40px', color: '#00ff41' }} />
+            <WhatsAppOutlined style={{fontSize: '40px', color: '#9A3D3D'}}/>
           </a>
+            <a
+                href='tel:+447760259130'
+                className='wp-icon'
+                id='wpIcon'
+                target='_blank'
+                rel='noopener noreferrer'
+            >
+              <PhoneOutlined style={{fontSize: '40px', color: '#9A3D3D'}}/>
+            </a>
         </div>
       </div>
     </header>
-  );
+);
 };
